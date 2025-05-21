22bcf2c6f636d1624af82dc8c5b64caefd53d5ae
baodeptry02 <aqaq03122003@gmail.com>
2025-05-21 17:15:03 +0700

    add some features

--- router/produciRoute.js
+++ router/produciRoute.js
@@ -8,5 +8,6 @@ router.post("/create", ProductController.createProduct)
 router.get("/all", ProductController.getAllProducts)
 
 router.get("/:id", ProductController.getProductById)
+router.post('/address', ProductController.getAddressData);
 
 module.exports = router;
