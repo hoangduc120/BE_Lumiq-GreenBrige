d5db38de9f42e2425ad8f57fdb0a9e4f470dc5be
hoangduc120 <hoangduc05122003@gmail.com>
2025-06-02 23:45:16 +0700

    fix product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,37 +3,6 @@ const { OK, BAD_REQUEST } = require("../configs/response.config");
 
 
 class ProductController {
-    async createProduct(req, res) {
-        try {
-            const product = await productSevice.createProduct(req.body)
-            return OK(res, "Product created successfully", { product });
-        } catch (error) {
-            return BAD_REQUEST(res, error.message);
-        }
-    }
-
-    async getAllProducts(req, res) {
-        try {
-            const { page, limit, sort, search } = req.query;
-
-            // Service trả về { products, totalProducts, totalPages, currentPage }
-            const productsData = await productSevice.getAllProducts(page, limit, sort, search);
-
-            // Trả về kết quả với cấu trúc đúng
-            return OK(res, "Get all products successfully", productsData);
-        } catch (error) {
-            return BAD_REQUEST(res, error.message);
-        }
-    }
-
-    async getProductById(req, res) {
-        try {
-            const product = await productSevice.getProductById(req.params.id)
-            return OK(res, "Get product by id successfully", { product });
-        } catch (error) {
-            return BAD_REQUEST(res, error.message);
-        }
-    }
 
     async getAddressData (req, res) {
         const { province, district, ward_street } = req.body;
